12af76528ca8f31504189cfc4a7b62997ef504c2
DaviEmanuelBackondeAraujo <103202792+DaviEmanuelBackondeAraujo@users.noreply.github.com>
2023-10-26 09:00:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-function tocaSom(idElementoAudio){
+function tocaSomPom(){
     document.querySelector(idElementoAudio).play('');
 }
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0; 
+listaDeTeclas[0].onclick = tocaSomPom;
 
-while (contador < 9) {
-    listaDeTeclas[contador].onclick = function(){
-        tocaSom('#som_tecla_clap'); 
+for (contador = 0; contador < listaDeTeclas.length; contador++) {
+    const instrumento = listaDeTeclas[contador].classList[1];
+
+    const tecla = listaDeTeclas[contador];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSom(idAudio); 
     };
-       
-    contador = contador + 1;
+
 }
 listaDeTeclas[1].onclick = TocaSomClap;
 listaDeTeclas[2].onclick = TocasomTim;
